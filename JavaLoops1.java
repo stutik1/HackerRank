0a71879953fbe5b039e7bc2fe662b559b2e7e7df
stuti <stutimegha2019@gmail.com>
2022-02-02 14:45:47 +0530

    Solution

--- JavaLoops1.java
+++ JavaLoops1.java
@@ -1,13 +1,12 @@
 //https://www.hackerrank.com/challenges/java-loops-i/problem
+
 import java.util.*;
 
 public class JavaLoops1 {
     private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         int N = scanner.nextInt();
-        for(int i =1; i<=10; i++)
-        {
+        for(int i =1; i<=10; i++) {
             System.out.println(N + " x " + i + " = " + N*i);
         }
     }

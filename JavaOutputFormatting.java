be0ea3e30526934675d2c5463b91fe008c7ed144
stuti <stutimegha2019@gmail.com>
2022-02-02 14:31:46 +0530

    Solution

---
+++ JavaOutputFormatting.java
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/java-output-formatting/problem
+
+import java.util.Scanner;
+public class JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf( "%-15s", s1);
+            System.out.printf( "%03d%n", x);
+        }
+        System.out.println("================================");
+    }
+}
